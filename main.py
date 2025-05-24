f62d10fcd555686739a5be7c7de0ab1d57d05eee
Lokyj <javier.orestes@mail.udp.cl>
2025-05-24 13:15:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,13 @@ def predecir(data: EntradaModelo):
     # Convertir datos a matriz y hacer predicción
     entrada = np.array([[data.edad, data.fc_reposo, data.fc_promedio, data.imc, data.dias_entrenando]])
     prediccion = modelo.predict(entrada)
+    umbrales = [0.1, 0.005, 0.005, 0.005, 0.005, 0.4]
+
+    # Aplicar umbrales para obtener predicción binaria
+    pred_binaria = (prediccion > umbrales).astype(int)
     # Convertimos a lista de floats con redondeo
-    return {"prediccion": prediccion[0].tolist()}
+    return {"prediccion": prediccion[0].tolist(),
+            "prediccion_binaria": pred_binaria[0].tolist()}
 
 @app.get("/")
 def saludo():
