daff883bc957605a7a796c499542d3bd474300e5
Lokyj <javier.orestes@mail.udp.cl>
2025-07-02 02:54:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def predecir(data: EntradaModelo):
     prediccion = modelo.predict(entrada_scaled)
 
     # Definir umbrales para cada etiqueta
-    umbrales = [0.5, 0.3, 0.25, 0.5, 0.25, 0.55]
+    umbrales = [0.5, 0.3, 0.25, 0.5, 0.27, 0.55]
 
     # Aplicar umbrales para obtener predicción binaria
     pred_binaria = (prediccion > umbrales).astype(int)
